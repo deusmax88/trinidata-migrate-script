10b90f596ac9e55465a6a258481dc0512a9ce45e
Mark Pasko <deusmax88@gmail.com>
2019-07-28 22:36:35 +0500

    Multiple object types with attributes processing added

--- trinidata.php
+++ trinidata.php
@@ -5,61 +5,54 @@ error_reporting(E_ALL);
 require_once("MDMRequest.php");
 require_once('MDMClient.php');
 
-/**
- * Карта атрибутов
- */
-$attributeIdsMap = [
-    'Руководитель' => 'ФИОРуководителя'
-];
-
-/**
- * Целовой класс объектов
- */
-$targetTypeId =
-    "ДетскиеБольницы"
-//    "УчреждениеЗдравоохранения"
-;
-
 $mdmClient = new MDMClient(
-"http://demo.trinidata.ru/mdm/rest/",
-"test",
-"demo"
+    "http://demo.trinidata.ru/mdm/rest/",
+    "test",
+    "demo"
 );
 
-$limit = 100;
-$offset = 0;
-$total = $mdmClient->getNumObjectsOfType($targetTypeId);
-$pages = intdiv($total, $limit) + ($total % $limit ? 1 : 0);
-$page = 0;
+$replacements = [
+    'ДетскиеБольницы' => [
+        'Руководитель' => 'ФИОРуководителя'
+    ]
+];
 
-do {
-    $objects = $mdmClient->getObjectsOfType($targetTypeId, $offset, $limit);
-    foreach ($objects as $object) {
+foreach($replacements as $targetTypeId => $attributeIdsMap) {
+    $limit = 100;
+    $offset = 0;
+    $total = $mdmClient->getNumObjectsOfType($targetTypeId);
+    $pages = intdiv($total, $limit) + ($total % $limit ? 1 : 0);
+    $page = 0;
 
-        foreach ($object['Attribute'] as $attribute) {
-            $oldAttributeId = $attribute['AttributeId'];
-            $targetAttributeId = $attributeIdsMap[$oldAttributeId] ?? null;
-            if (!$targetAttributeId) {
-                continue;
+    do {
+        $objects = $mdmClient->getObjectsOfType($targetTypeId, $offset, $limit);
+        foreach ($objects as $object) {
+
+            foreach ($object['Attribute'] as $attribute) {
+                $oldAttributeId = $attribute['AttributeId'];
+                $targetAttributeId = $attributeIdsMap[$oldAttributeId] ?? null;
+                if (!$targetAttributeId) {
+                    continue;
+                }
+
+                $type = $attribute['Type'];
+                $value = $attribute['Value'];
+                $entityCode = $object['Code'];
+
+                // Для атрибута ссылки сбросим предыдцщее значение
+                if ($type == "Reference") {
+                    $mdmClient->updateEntityAttribute($targetTypeId, $entityCode, $type, $oldAttributeId, null, true);
+                }
+
+                $mdmClient->updateEntityAttribute($targetTypeId, $entityCode, $type, $targetAttributeId, $value);
             }
 
-            $type = $attribute['Type'];
-            $value = $attribute['Value'];
-            $entityCode = $object['Code'];
-
-            // Для атрибута ссылки сбросим предыдцщее значение
-            if ($type == "Reference") {
-                $mdmClient->updateEntityAttribute($targetTypeId, $entityCode, $type, $oldAttributeId, null, true);
-            }
-
-            $mdmClient->updateEntityAttribute($targetTypeId, $entityCode, $type, $targetAttributeId, $value);
         }
-
+        $page++;
+        $offset = $page * $limit;
     }
-    $page++;
-    $offset = $page * $limit;
+    while($page < $pages);
 }
-while($page < $pages);
 
 //$entityCode = 'ДетскиеБольницы_a01fdbc76de1270edeb3915cdb441c6f';
 //var_dump($mdmClient->getNumObjectsOfType($targetTypeId));
